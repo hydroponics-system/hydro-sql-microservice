6e494086c4d8d514ee98a9ff3eb9daf3eb482b2a
Samuel Butler <sambutler1017@icloud.com>
2022-08-28 14:45:03 -0400

    no message

--- src/main/java/com/hydro/HydroSqlApplication.java
+++ src/main/java/com/hydro/HydroSqlApplication.java
@@ -3,12 +3,12 @@ package com.hydro;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Empty main for building out jar file.
+ * Empty main for building out jar file for SQL microservice.
  * 
  * @author Sam Butler
  * @since August 27, 2022
  */
-public class HydroCommonApplication {
+public class HydroSqlApplication {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {}
 }
